ff03251eaf5716bac69626e2cb5d264bcbf27dc3
yuanlizhanshi <826907860@qq.com>
2023-03-18 21:21:38 +0800

    Update Preprocess_sample1_scATAC-seq.R

--- 1.Data_format_normalization/Preprocess_sample1_scATAC-seq.R
+++ 1.Data_format_normalization/Preprocess_sample1_scATAC-seq.R
@@ -7,4 +7,4 @@ merged_matrix <- ReadMtx('../ignore/GSE165837_CARE_ATAC_merged_matrix.mtx.gz',
                          feature.column = 1)
 label <- read.delim("E:/DRCTdb/ignore/GSE165837_CARE_ATAC_merged_umap_cluster_labels.tsv.gz", row.names=1)
 obj <- CreateSeuratObject(merged_matrix,meta.data = label)
-save(merged_matrix,file = '../data/scATAC-seq/sample1/merged_matrix.Rds')
+saveRDS(merged_matrix,file = '../data/scATAC-seq/sample1/merged_matrix.Rds')
