5d079757761996f3d971401a7cf5f93a2c43a844
yuanlizhanshi <826907860@qq.com>
2023-03-21 10:25:40 +0800

    Create Preprocess_sample1_scRNA-seq.R

---
+++ 1.Data_format_normalization/Preprocess_sample1_scRNA-seq.R
@@ -0,0 +1,17 @@
+### Transfer data to seurat object
+library(Seurat)
+library(tidyverse)
+merged_matrix <- ReadMtx(mtx = './rawdata/GSE165838_CARE_RNA_counts.mtx.gz',
+                         cells = './rawdata/GSE165838_CARE_RNA_barcodes.txt.gz',cell.column = 2, skip.cell = 1,
+                         features = './rawdata/GSE165838_CARE_RNA_features.tsv.gz',feature.column = 2,skip.feature = 1)
+metadata <- read.delim("./rawdata/GSE165838_CARE_RNA_metadata.txt.gz", row.names=1)
+obj <- CreateSeuratObject(merged_matrix,meta.data = metadata)
+
+
+obj <- SCTransform(obj) %>% RunPCA(verbose = FALSE) %>% 
+  RunUMAP(dims = 1:30,umap.method = 'umap-learn',metric = 'correlation',verbose = FALSE) %>%
+  FindNeighbors(dims = 1:30, verbose = FALSE) %>%
+  FindClusters(verbose = FALSE)
+saveRDS(obj,file = 'Rds/sce.Rds')
+
+
