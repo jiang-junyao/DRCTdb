b443d7ff283ff8878721bbfb1b502edddaf8ff7c
yuanlizhanshi <826907860@qq.com>
2023-03-24 18:24:43 +0800

    Generate seurat obj from peak by cell file

--- 1.Data_format_normalization/Preprocess_sample1_scATAC-seq.R
+++ 1.Data_format_normalization/Preprocess_sample1_scATAC-seq.R
@@ -1,10 +1,24 @@
 ### Transfer data to seurat object
 library(Seurat)
-
-merged_matrix <- ReadMtx('../ignore/GSE165837_CARE_ATAC_merged_matrix.mtx.gz',
-                         cells = '../ignore/GSE165837_CARE_ATAC_merged_barcodes.txt.gz',
-                         features = '../ignore/GSE165837_CARE_ATAC_merged_features.txt.gz',
+library(Signac)
+merged_matrix <- ReadMtx('../../data/scATAC-seq/sample1/GSE165837_CARE_ATAC_merged_matrix.mtx.gz',
+                         cells = '../../data/scATAC-seq/sample1/GSE165837_CARE_ATAC_merged_barcodes.txt.gz',
+                         features = '../../data/scATAC-seq/sample1/GSE165837_CARE_ATAC_merged_features.txt.gz',
                          feature.column = 1)
-label <- read.delim("E:/DRCTdb/ignore/GSE165837_CARE_ATAC_merged_umap_cluster_labels.tsv.gz", row.names=1)
-obj <- CreateSeuratObject(merged_matrix,meta.data = label)
-saveRDS(merged_matrix,file = '../data/scATAC-seq/sample1/merged_matrix.Rds')
+label <- read.delim("../../data/scATAC-seq/sample1/GSE165837_CARE_ATAC_merged_umap_cluster_labels.tsv.gz", row.names=1)
+
+chrom_assay <- CreateChromatinAssay(
+  counts = merged_matrix,
+  sep = c(":", "-"),
+  genome = 'hg38',
+  min.cells = 10,
+  min.features = 200
+)
+sample1 <- CreateSeuratObject(
+  counts = chrom_assay,
+  assay = "peaks",
+  meta.data = label
+)
+sample1$cell_type <- sample1$Cluster
+saveRDS(sample1,file = '../../data/scATAC-seq/sample1/Rds/sample1_seurat_obj.Rds')
+
