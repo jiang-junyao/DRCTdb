279d3633a31d1de9d85ddc8357897feda305baa2
jiang_junyao <jiangjunyao789@163.com>
2023-03-25 21:37:14 +0800

    Update preprocess.R

--- 2.Data preprocessing/preprocess.R
+++ 2.Data preprocessing/preprocess.R
@@ -4,7 +4,15 @@ find_DBRs <- function(obj,test_method = 'wilcox'){
   library(GenomicRanges)
   obj <- RunTFIDF(obj)
   DBRs <- FindAllMarkers(obj,test.use = test_method)
-  DBRs_gr <- GRanges(rownames(DBRs))
+  str_peak <- length(strsplit(DBRs$gene[1],'-')[[1]])
+  if (str_peak == 2) {
+    DBRs_gr <- GRanges(rownames(DBRs))
+  }else if(str_peak == 3){
+    peak_name = t(as.data.frame(strsplit(DBRs$gene,'-')))
+    peak_name = paste0(peak_name[,1],':',peak_name[,2],'-',peak_name[,3])
+    DBRs_gr <- GRanges(peak_name)
+  }
+  
   DBRs_gr$cluster <- DBRs$cluster
   DBRs_gr$p_adj <- DBRs$p_val_adj
   DBRs_gr$log2fc <- DBRs$avg_log2FC
