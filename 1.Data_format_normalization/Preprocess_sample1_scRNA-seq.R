4a3fc3caedb93a0757928be9fea0a36e918eaa3c
yuanlizhanshi <826907860@qq.com>
2023-04-01 17:26:39 +0800

    Reprocess sample1

--- 1.Data_format_normalization/Preprocess_sample1_scRNA-seq.R
+++ 1.Data_format_normalization/Preprocess_sample1_scRNA-seq.R
@@ -1,17 +1,12 @@
 ### Transfer data to seurat object
 library(Seurat)
 library(tidyverse)
-merged_matrix <- ReadMtx(mtx = './rawdata/GSE165838_CARE_RNA_counts.mtx.gz',
-                         cells = './rawdata/GSE165838_CARE_RNA_barcodes.txt.gz',cell.column = 2, skip.cell = 1,
-                         features = './rawdata/GSE165838_CARE_RNA_features.tsv.gz',feature.column = 2,skip.feature = 1)
-metadata <- read.delim("./rawdata/GSE165838_CARE_RNA_metadata.txt.gz", row.names=1)
+merged_matrix <- ReadMtx(mtx = '../../data/scRNA-seq/sample1/GSE165838_CARE_RNA_counts.mtx.gz',
+                         cells = '../../data/scRNA-seq/sample1/GSE165838_CARE_RNA_barcodes.txt.gz',cell.column = 2, skip.cell = 1,
+                         features = '../../data/scRNA-seq/sample1/GSE165838_CARE_RNA_features.tsv.gz',feature.column = 2,skip.feature = 1)
+metadata <- read.delim("../../data/scRNA-seq/sample1/GSE165838_CARE_RNA_metadata.txt.gz", row.names=1)
 obj <- CreateSeuratObject(merged_matrix,meta.data = metadata)
 
-
-obj <- SCTransform(obj) %>% RunPCA(verbose = FALSE) %>% 
-  RunUMAP(dims = 1:30,umap.method = 'umap-learn',metric = 'correlation',verbose = FALSE) %>%
-  FindNeighbors(dims = 1:30, verbose = FALSE) %>%
-  FindClusters(verbose = FALSE)
-saveRDS(obj,file = 'Rds/heart_processed.Rds')
+saveRDS(obj,file = '../../data/scRNA-seq/sample1/sample1_heart_scRNA_35k_processed.Rds')
 
 
