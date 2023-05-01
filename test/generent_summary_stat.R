a32c0ed4468e3bf56ab87dc0aca6e35671aeb4b2
yuanlizhanshi <826907860@qq.com>
2023-05-01 18:28:55 +0800

    Add full summary statistics

--- test/generent_summary_stat.R
+++ test/generent_summary_stat.R
@@ -11,3 +11,29 @@ data.table::fwrite(df,'Summary.Statistics.xls',sep = '\t')
 new_file <- data.frame(new_file = list.files('../../LDSC_hg38/summary_statistics/AlkesGroup/'))
 
 data.table::fwrite(new_file,'Summary.Statistics2.xls',sep = '\t')
+
+df <- readxl::read_excel('Summary.Statistics.xls',sheet = 'Sheet1')
+df2 <- df %>% filter(!is.na(trait)) %>% select(1:2)
+
+df2$file <- paste0('../summary_statistics/AlkesGroup/',df2$file)
+data.table::fwrite(df2,'Summary.Statistics2.xls',sep = '\t')
+df <- readxl::read_excel('Summary_Statistics.xlsx')
+df2 <- df %>% distinct(.keep_all = T)
+
+writexl::write_xlsx(df2,'Summary_Statistics2.xlsx')
+
+
+
+df <- readxl::read_excel('Summary_Statistics.xlsx',sheet = 'Sheet1') %>% 
+    select(2:1) %>% distinct(trait,.keep_all = T) %>% filter(!(trait %in% df2$trait))
+df$new_file <- paste0('../summary_statistics/GWAS_Catalog/',df$new_file)
+
+
+
+writexl::write_xlsx(df,'Summary_Statistics3.xlsx')
+
+df2 <- readxl::read_excel('Summary_Statistics.xlsx',sheet = 'Sheet2')
+
+
+
+
