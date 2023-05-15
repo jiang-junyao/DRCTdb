56bfe7c5bf09d2669039efe56e1485812726cb01
yuanlizhanshi <826907860@qq.com>
2023-05-15 23:13:47 +0800

    Update Preprocess_sample3_bed.R

--- 1.Data_format_normalization/Preprocess_sample3_bed.R
+++ 1.Data_format_normalization/Preprocess_sample3_bed.R
@@ -21,28 +21,30 @@ cell_gr_list <- purrr::map(merged_matrix_df,function(x){
 cell_type <- tibble(
   raw_cell_type = names(cell_gr_list),
   new_cell_type = map_chr(names(cell_gr_list),function(x){
-  if (is.na(str_extract(x,'.*(?=\\s\\d)'))) {
-    res <- x
-  }else{
-    res <- str_extract(x,'.*(?=\\s\\d)')
-  }
+    res <- str_replace_all(x,' ', '_')
+    return(res)
 }))
 
-merged_cell_type <- unique(cell_type$new_cell_type)
+names(cell_gr_list) <- cell_type$new_cell_type
 
 
-reduced_cell_type <- map(seq_along(merged_cell_type),function(i){
-  concensus_gr <- GenomicRanges::reduce(reduce(cell_gr_list[str_which(names(cell_gr_list),fixed(merged_cell_type[i]))],c))
-  return(concensus_gr)
-}) %>% setNames(merged_cell_type)
+
+  
+sample3 <- cell_gr_list[-str_which(names(cell_gr_list),'Fetal')]
 
 
-names(reduced_cell_type) <- str_replace(names(reduced_cell_type),'\\+','_')
 
-for (i in 1:length(reduced_cell_type)) {
-  filenames <- paste0('../../data/bed/sample3/',names(reduced_cell_type)[i],'.bed.gz')
-  rtracklayer::export.bed(object = reduced_cell_type[[i]],con = filenames)
-  cat(names(reduced_cell_type)[i],'\n')
+for (i in 1:length(sample3)) {
+  filenames <- paste0('../../data/bed/sample3/',names(sample3)[i],'.bed.gz')
+  rtracklayer::export.bed(object = sample3[[i]],con = filenames)
+  cat(names(sample3)[i],'\n')
 }
 
+sample5 <- cell_gr_list[str_which(names(cell_gr_list),'Fetal')]
+
+for (i in 1:length(sample5)) {
+  filenames <- paste0('../../data/bed/sample5/',names(sample5)[i],'.bed.gz')
+  rtracklayer::export.bed(object = sample5[[i]],con = filenames)
+  cat(names(sample5)[i],'\n')
+}
 
