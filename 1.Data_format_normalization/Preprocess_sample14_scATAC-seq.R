bc4e2aac4b5d43f8db34190f42178492c9424a3d
yuanlizhanshi <826907860@qq.com>
2023-06-18 16:15:15 +0800

    Update Preprocess_sample14_scATAC-seq.R

--- 1.Data_format_normalization/Preprocess_sample14_scATAC-seq.R
+++ 1.Data_format_normalization/Preprocess_sample14_scATAC-seq.R
@@ -4,14 +4,15 @@ library(tidyverse)
 #FCA_GND8046539
 
 load_matrix <- function(x){
-  mrtrix = paste0('../../../../Desktop/Sample14/E-MTAB-10570/',x,'_matrix.mtx.gz')
-  feature = paste0('../../../../Desktop/Sample14/E-MTAB-10570/',x,'_barcodes.tsv.gz')
-  cells = paste0('../../../../Desktop/Sample14/E-MTAB-10570/',x,'_features.tsv.gz')
+  mrtrix = paste0('../../data/scATAC-seq/Sample14/E-MTAB-10570/',x,'_matrix.mtx.gz')
+  feature = paste0('../../data/scATAC-seq/Sample14/E-MTAB-10570/',x,'_barcodes.tsv.gz')
+  cells = paste0('../../data/scATAC-seq/Sample14/E-MTAB-10570/',x,'_features.tsv.gz')
   merged_matrix <- ReadMtx(mtx = mrtrix,
                            features = feature,
                            cells = cells,
                            feature.column = 1)
   merged_matrix <- Matrix::t(merged_matrix)
+  colnames(merged_matrix) <- paste0(x,'#',colnames(merged_matrix))
   chrom_assay <- CreateChromatinAssay(
     counts = merged_matrix,
     sep = c(":", "-"),
@@ -22,15 +23,15 @@ load_matrix <- function(x){
   return(chrom_assay)
 }
 
-all_file <- unique(na.omit(str_extract(list.files('../../../../Desktop/Sample14/E-MTAB-10570/'),'.*(?=_)')))
+all_file <- unique(na.omit(str_extract(list.files('../../data/scATAC-seq/Sample14/E-MTAB-10570/'),'.*(?=_)')))
 
 chrom_assay_list <- map(all_file,load_matrix)
 
-chrom_assay <- reduce(chrom_assay_list,merge)
+chrom_assay <- purrr::reduce(chrom_assay_list,merge)
 
 sample14 <- CreateSeuratObject(
   counts = chrom_assay,
   assay = "peaks"
 )
 
-saveRDS(sample13,'../../../../Desktop/Sample14/peak_matrix.Rds')
+saveRDS(sample14,'../../data/scATAC-seq/Sample14/peak_matrix.Rds')
