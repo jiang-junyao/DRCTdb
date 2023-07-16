14b49d58cbf37075dad9e0f31863ed8552b95caf
yuanlizhanshi <826907860@qq.com>
2023-07-16 19:59:24 +0800

    Update Generate_sample23_bed.R

--- 2.Data preprocessing/Generate_sample23_bed.R
+++ 2.Data preprocessing/Generate_sample23_bed.R
@@ -4,16 +4,15 @@ library(tidyverse)
 library(Matrix)
 library(glue)
 source('preprocess_functions.R')
-sample23_ATAC <- readRDS('../../data/scATAC-seq/Sample23/sample23_scATAC-seq.Rds')
-
-sparse_mtx <- sample23_ATAC@assays$peaks@counts
-rownames(sparse_mtx) <- paste0('chr',rownames(sparse_mtx))
-
-##subset columns, if your peak is less than 1M, your does not need to use multithreads
+###
+sample23_ATAC <- readRDS('../../data/scATAC-seq/sample23/islet_multiome_seurat_singac.Rds')
+DefaultAssay(sample23_ATAC) <- 'ATAC'
+sparse_mtx <- sample23_ATAC@assays$ATAC@counts
+sparse_mtx <- sparse_mtx[str_starts(rownames(sparse_mtx),'chr'),]
 
 pseudobulk <- generate_pseudobulk(sparse_mtx,group_by = sample23_ATAC$cell_type)
-
 cell_gr <- separate(as.data.frame(rownames(pseudobulk)),col = everything(),sep = '-',into = c('seqnames','start','end'))
+
 cell_gr_list <- map(colnames(pseudobulk),get_cell_gr,seurat_Obj = sample23_ATAC)
 names(cell_gr_list) <- colnames(pseudobulk)
 
