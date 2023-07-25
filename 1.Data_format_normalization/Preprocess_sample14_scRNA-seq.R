b9569a9c8707d3ae3c93fd456c1a105d8c588f29
yuanlizhanshi <826907860@qq.com>
2023-07-25 20:04:29 +0800

    Update Preprocess_sample14_scRNA-seq.R

--- 1.Data_format_normalization/Preprocess_sample14_scRNA-seq.R
+++ 1.Data_format_normalization/Preprocess_sample14_scRNA-seq.R
@@ -24,32 +24,42 @@ load_matrix <- function(x,path = '../../data/scATAC-seq/Sample14/E-MTAB-10570/')
   return(rna_seurat)
 }
 
+
+all_file <- unique(na.omit(str_extract(list.files('../../data/scRNA-seq/Sample14/E-MTAB-10551/'),'.*(?=_)')))
+
+metdata_files <- list.files('../../data/scATAC-seq/Sample14/metadata/',full.names = T)
+
 map(all_file,function(x){
   feature = paste0('../../data/scRNA-seq/Sample14/E-MTAB-10551/',x,'_features.tsv.gz')
   data.table::fread(feature) %>% nrow()
 })
 
-metdata_list <- map_dfr(metdata_files,data.table::fread) 
-
-
-metdata_list$barcorde <- str_extract(metdata_list$V1,'(?<=\\d_).*')
-metdata_list$cells <- paste0(metdata_list$sample,'#', metdata_list$barcorde, '-1') 
-metdata_list <- metdata_list %>% distinct(cells,.keep_all = T)
-sample14_cells <-metdata_list$cells %>% intersect(colnames(sce)) 
 
 all_file <- unique(na.omit(str_extract(list.files('../../data/scRNA-seq/Sample14/E-MTAB-10551/'),'.*(?,=_)')))
 sce_list <- map(all_file[c(-9)][-21][-60],load_matrix,path = '../../data/scRNA-seq/Sample14/E-MTAB-10551/',.progress = T)
 sce <- purrr::reduce(sce_list,merge)
 
+metdata_list <- map_dfr(metdata_files,data.table::fread)
+metdata_list$barcorde <- str_extract(metdata_list$V1,'(?<=\\d_).*')
+metdata_list$cells <- paste0(metdata_list$sample,'#', metdata_list$barcorde, '-1')
+metdata_list <- metdata_list %>% distinct(cells,.keep_all = T)
 
-sample14_rna <- sce[,cells]
-sample14_medadata <- metdata_list %>% filter(cells %in% sample14_cells)
+sample14_cells <- metdata_list$cells %>% intersect(colnames(sce))
 
-rownames(sample14_medadata) <- sample14_medadata$sample14_cells
-sample14_medadata <- as.data.frame(sample14_medadata)
+
+sample14_rna <- sce[,sample14_cells]
+sample14_medadata <- metdata_list %>% filter(cells %in% sample14_cells) %>% as.data.frame()
+
+rownames(sample14_medadata) <- sample14_medadata$cells
+
 sample14_medadata <- sample14_medadata[colnames(sample14_rna),]
 
+identical(colnames(sample14_rna),rownames(sample14_medadata))
 
 sample14_rna <- sample14_rna %>% AddMetaData(metadata = sample14_medadata)
-sample14_rna$cell_type <- sample14_rna$celltype
+
+sample14_rna$cell_type <- sample14_rna$lineage
 saveRDS(sample14_rna,'../../data/scRNA-seq/Sample14/sample14_processed_300k_scRNA-seq.Rds')
+
+
+
