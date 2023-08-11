640787b88c21d8a2dc4eb115766e3deba3b49836
yuanlizhanshi <826907860@qq.com>
2023-08-11 10:16:23 +0800

    Update Preprocess_sample23_multiome.R

--- 1.Data_format_normalization/Preprocess_sample23_multiome.R
+++ 1.Data_format_normalization/Preprocess_sample23_multiome.R
@@ -1,40 +1,28 @@
 ### Transfer data to seurat object
 library(Seurat)
 library(Signac)
-label <- data.table::fread("../../data/scATAC-seq/Sample23/multiome/GSE200044_multiome_beta_annotation.csv.gz") |> as.data.frame()
-rownames(label) <- label$X
-label <- label[,c(-1,-2)]
-#scRNA-seq
-merged_matrix_RNA <- ReadMtx('../../data/scATAC-seq/Sample23/multiome/beta_RNA.mtx',
-                         features = '../../data/scATAC-seq/Sample23/multiome/beta_RNA.barcodes',
-                         cells = '../../data/scATAC-seq/Sample23/multiome/beta_RNA.genes',
-                         feature.column = 1)
-merged_matrix_RNA <- Matrix::t(merged_matrix_RNA)
-obj <- CreateSeuratObject(merged_matrix_RNA,meta.data = label)
+library(tidyverse)
+source('../2.Data preprocessing/preprocess_functions.R')
+sample23_mtx <- readRDS('../../data/scATAC-seq/sample23/islet_reproducible_peak_matrix.Rds')
 
-saveRDS(merged_matrix_RNA,'../../data/scRNA-seq/Sample23/sample23_scRNAseq.Rds')
-
-#scATAC-seq
-merged_matrix_ATAC <- ReadMtx('../../data/scATAC-seq/Sample23/multiome/beta_ATAC_500bp.mtx',
-                         features = '../../data/scATAC-seq/Sample23/multiome/beta_ATAC_500bp.barcodes',
-                         cells = '../../data/scATAC-seq/Sample23/multiome/beta_ATAC_500bp.regions',
-                         feature.column = 1)
-merged_matrix_ATAC <- Matrix::t(merged_matrix_ATAC)
-
-
+sparse_mtx <- sample23_mtx@assays@data$PeakMatrix
+rownames(sparse_mtx) <- paste(as.data.frame(sample23_mtx@rowRanges)[[1]],as.data.frame(sample23_mtx@rowRanges)[[2]],as.data.frame(sample23_mtx@rowRanges)[[3]],sep = '-')
+sparse_mtx <- sparse_mtx[which(map_vec(rownames(sparse_mtx),subset_peaks)),]
 chrom_assay <- CreateChromatinAssay(
-  counts = merged_matrix_ATAC,
-  sep = c(":", "-"),
-  genome = 'hg19',
+  counts = sparse_mtx,
+  sep = c("-", "-"),
+  genome = 'hg38',
   min.cells = 10,
   min.features = 200
 )
-sample23 <- CreateSeuratObject(
+sample23_ATAC <- CreateSeuratObject(
   counts = chrom_assay,
   assay = "peaks",
-  meta.data = label
+  meta.data = as.data.frame(sample23_mtx@colData)
 )
-sample23$cell_type <- sample23$subtype
-saveRDS(sample23,file = '../../data/scATAC-seq/sample23/sample23_scATAC-seq.Rds')
+saveRDS(sample23_ATAC,file = '../../data/scATAC-seq/sample23/sample23_scATAC-seq_95k_processed.Rds')
+
+
+
 ##
 
